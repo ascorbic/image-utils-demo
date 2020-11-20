e190cde94d1733a5992fde8ca73ff12967bd52a1
Matt Kane <matt@gatsbyjs.com>
2020-11-20 13:38:05 +0000

    Fix import

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,6 @@
 import { GatsbyImage } from "gatsby-plugin-image";
 import React from "react";
-import { useCloudinaryImage } from "./use-cloudinary-image";
+import { useCloudinaryImage } from "../use-cloudinary-image";
 
 export default function Index(): JSX.Element {
   const image = useCloudinaryImage({
